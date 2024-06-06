ae50789dc59dfc6647b3d2719d8b4d8ff18fc29d
josterga <josterga@uci.edu>
2024-06-05 23:48:19 -0700

    more text adjustments, spacing

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3763862647"
+    serviceWorkerVersion: "2769728538"
   }
 });
